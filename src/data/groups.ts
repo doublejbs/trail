50ace6413db050c7f7ed8025dd4d85300dcb765e
서진용[Storage Service FE] <jinyong.suh@navercorp.com>
2026-03-17 23:44:55 +0900

    feat: add dummy group data

---
+++ src/data/groups.ts
@@ -0,0 +1,12 @@
+export interface Group {
+  id: number;
+  name: string;
+}
+
+export const DUMMY_GROUPS: Group[] = [
+  { id: 0, name: '한라산 팀' },
+  { id: 1, name: '설악산 팀' },
+  { id: 2, name: '지리산 팀' },
+  { id: 3, name: '북한산 팀' },
+  { id: 4, name: '태백산 팀' },
+];
